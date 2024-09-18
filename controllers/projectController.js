48f485c68f166d310424648df06c147843d29411
masouda1997 <masoud30376@gmail.com>
2024-09-18 11:28:35 +0330

    fix the output format to the desired foramt is want with sequelize

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -8,14 +8,24 @@ const getAllProjects = async(req, res) => {
 const getProjectSkills = async(req,res) => {
    try {
       const projects = await model.projects.findAll({
+         attributes: ['name'], // Only select the project name
          include:{
             model:model.skills,
+            attributes: ['title'], // Only select the skill title
+            through: { attributes: [] } // Exclude the join table fields
          }
       })
-      if(!projects){
-         return res.status(404).send({message:"skills not found!"})
+      if(!projects || projects.length === 0){
+         return res.status(404).send({message:"project or skills not found!"})
       }
-      return res.status(200).send(projects)
+
+      // format the response 
+      const formattedList  = projects.map(p => ({
+         project : p.name,
+         skills : p.Skills.map(s => s.title)
+      }))
+
+      return res.status(200).send(formattedList)
    } catch (error) {
       console.log("error getting project skills" , error);
       res.status(500).send({message:"Internal server ERROR"})
